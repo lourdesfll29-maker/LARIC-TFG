83cb5011760b82f3b8e54b0b397cec32ddfd624b
Lourdes <lourdesfll29@gmail.com>
2026-05-21 19:47:52 +0200

    Updated folders organization

--- laric_ws/src/laric_core/setup.py
+++ laric_ws/src/laric_core/setup.py
@@ -5,7 +5,7 @@ package_name = 'laric_core'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -14,17 +14,12 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='lourdes',
-    maintainer_email='lourdes@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='lourdesfll29@gmail.com',
+    description='LARIC: Language-based Agent for Robotic Interaction and Control',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            
+            # Intentionally left blank for Bash script execution
         ],
     },
 )
